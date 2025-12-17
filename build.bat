c981bfe6c8059ab44198cc908c50dcb76283f641
kordybordy <132927916+kordybordy@users.noreply.github.com>
2025-12-17 22:37:09 +0100

    Update build.bat for Renamer application

--- build.bat
+++ build.bat
@@ -1,21 +1,30 @@
 @echo off
+
 cd /d %~dp0
 
+
+
 REM Clean previous build
-REM Make sure no running instance keeps dist\renamer.exe locked
-taskkill /f /im renamer.exe 2>nul
+REM Make sure no running instance keeps dist\Renamer.exe locked
+taskkill /f /im Renamer.exe 2>nul
 rmdir /s /q dist 2>nul
 rmdir /s /q build 2>nul
 del /q main.spec 2>nul
 
+
 pyinstaller ^
+
   --onefile ^
   --noconsole ^
-  --name renamer ^
+  --name Renamer ^
+  --icon "assets\logo.ico" ^
   --add-data "poppler;poppler" ^
   --add-data "tesseract;tesseract" ^
+  --add-data "assets;assets" ^
   main.py
 
+
 echo.
-echo Build complete. EXE is in "dist\renamer.exe"
+
+echo Build complete. EXE is in "dist\Renamer.exe"
 pause
